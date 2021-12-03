9f9a731a23626a966b0fef6b2b24a0c3385476a0
ODDINProgrammer <39472811+ODDINProgrammer@users.noreply.github.com>
2021-12-03 10:12:31 +0200

    Finished basic version
    Implemented height variation for land and sand blocks.
    Added some environment objects to make it more interesting.
    Added SFX.
    I see it as version 1.0 done.

---
+++ New Unity Project/Assets/Scripts/Tile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour
+{
+    public enum TileType { Sand, Water, Land }
+    public TileType Type;
+
+    [SerializeField] private List<GameObject> objectPrefabs;
+    [SerializeField] private List<int> chanceToSpawn;
+    [SerializeField] private Transform spawnPoint;
+
+    public void TrySpawningObject(int generatedNumber)
+    {
+        for (int i = 0; i < chanceToSpawn.Count; i++)
+        {
+            if (generatedNumber <= chanceToSpawn[i])
+            {
+                Instantiate(objectPrefabs[i], spawnPoint);
+                return;
+            }
+            else
+            {
+                generatedNumber -= chanceToSpawn[i];
+            }
+        }
+    }
+
+}
+
--- New Unity Project/Assets/Scripts/WorldGenerationV2.cs
+++ New Unity Project/Assets/Scripts/WorldGenerationV2.cs
@@ -1,49 +1,71 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class WorldGenerationV2 : MonoBehaviour
 {
+    [Header("Generation settings")]
     [SerializeField] private Vector2Int mapSize;
-    [SerializeField] private List<GameObject> tiles;
-    [Range(0.01f, 0.5f)][SerializeField] private float generationSpeed;
+    [SerializeField] private List<Tile> landPrefab;
+    [SerializeField] private List<GameObject> generatedTiles;
+    [Range(0.01f, 2f)] [SerializeField] private float generationSpeed;
 
-    private GameObject[,] spawnedTiles;
+    [Header("Accessors")]
+    [SerializeField] private Button generateButton;
 
     private Vector3 tileOffset = new Vector3(3, 0, 3);
+    private RaycastHit hit;
 
     public void GenerateRandom() { StartCoroutine(GenerateRandomMap()); }
-
+    //public void GenerateWeighted() { StartCoroutine(GenerateWeightedMap()); }
     public void ClearMap()
     {
-        foreach(Transform child in transform)
+        foreach (Transform child in transform)
         {
             Destroy(child.gameObject);
             StopAllCoroutines();
         }
-    }    
+
+        generatedTiles.Clear();
+    }
+
     //  GENERATES TILES RANDOMLY
     private IEnumerator GenerateRandomMap()
     {
-        Debug.Log("Starting new random map");
-        Vector2Int pos = new Vector2Int(0,0);
-        for(int y = 0; y < mapSize.y; y++)
+        Vector2Int pos = new Vector2Int(0, 0);
+        for (int y = 0; y < mapSize.y; y++)
         {
             pos.x = 0;
             pos.y -= 3;
-            for(int x = 0; x < mapSize.x; x++)
+            for (int x = 0; x < mapSize.x; x++)
             {
                 pos.x += 3;
                 yield return new WaitForSeconds(generationSpeed);
-                PlaceTile(pos.x, pos.y);
+                PlaceRandomTile(pos.x, pos.y, Random.Range(0, landPrefab.Count));
             }
         }
-
-        Debug.Log("Finished generating");
+        StartCoroutine(GenerateObjects());
     }
 
-    private void PlaceTile(int x, int y)
+    private void PlaceRandomTile(int x, int y, int tileIndex)
     {
-        Instantiate(tiles[Random.Range(0, tiles.Count)], new Vector3(x, 0, y), Quaternion.identity, transform);
+        Tile tile = Instantiate(landPrefab[tileIndex], new Vector3(x, 0, y), Quaternion.identity, transform);
+        generatedTiles.Add(tile.gameObject);
+        //  IF TILE IS NOT WATER CHANGE HEIGHT TO A RANDOM VALUE
+        if (tile.Type != Tile.TileType.Water)
+            tile.transform.position = new Vector3(x, Random.Range(0f, 1f), y);
+
+    }
+
+    private IEnumerator GenerateObjects()
+    {
+        foreach(GameObject go in generatedTiles)
+        {
+            int rand = Random.Range(0, 100 + 1);
+            yield return new WaitForSeconds(generationSpeed);
+            go.GetComponent<Tile>().TrySpawningObject(rand);
+        }
+        generateButton.gameObject.SetActive(true);
     }
 }
